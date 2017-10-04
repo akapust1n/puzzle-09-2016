eb84f90cee516cb1e940e6f2135ecd97d8b769d8
rashemihmih <mihmih96@gmail.com>
2017-10-04 04:21:15 +0300

    remove useless annotation

--- src/main/java/ru/mail/park/services/AccountService.java
+++ src/main/java/ru/mail/park/services/AccountService.java
@@ -1,11 +1,9 @@
 package ru.mail.park.services;
 
-import org.springframework.stereotype.Service;
 import ru.mail.park.model.UserProfile;
 
 import java.util.List;
 
-@Service
 public interface AccountService {
     void addUser(String login, String password, String email);
     UserProfile getUserByLogin(String login);
